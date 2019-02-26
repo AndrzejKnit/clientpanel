3e519319e00dabf4d5ed1e7d8ebe8bfd86d4b56e
AndrzejKnit <andrzej.knitter@gmail.com>
2019-02-26 12:00:38 +0100

    lesson 64: Client details component

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+//import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
 //import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { Client } from '../models/Client';
 import { Observable } from 'rxjs';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 
 @Injectable({
@@ -34,5 +36,19 @@ export class ClientService {
 
    newClient(client: Client) {
     this.clientsCollection.add(client);
+   }
+
+   getClient(id: string): Observable<Client> {
+    this.clientDoc = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDoc.snapshotChanges().pipe(map(action => {
+      if (action.payload.exists === false) {
+        return null;
+      } else {
+        const data = action.payload.data() as Client;
+        data.id = action.payload.id;
+        return data;
+      }
+    }));
+    return this.client;
    }
 }
