eb4010a388dd104c410f0a61bdbea069ec3ee73f
AndrzejKnit <andrzej.knitter@gmail.com>
2019-03-01 09:08:01 +0100

    Lesson 71: Settings service & register guard part 2

--- src/app/guards/register.guard.ts
+++ src/app/guards/register.guard.ts
@@ -4,22 +4,19 @@ import { SettingsService } from '../services/settings.service';
 import { map } from 'rxjs/operators';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class RegisterGuard implements CanActivate {
     constructor(
         private router: Router,
         private settingsService: SettingsService
-    ) {
+    ) {}
 
-    }
+    canActivate(): boolean {
 
-    canActivate(): Observable<boolean> {
-        return this.afAuth.authState.pipe(map(auth => {
-            if (!auth) {
+            if (this.settingsService.getSettings().allowRegistration) {
+                return true;
+            } else {
                 this.router.navigate(['/login']);
                 return false;
-            } else {
-                return true;
             }
-        }));
     }
 }
